08f3eb9ce4382689b977901f0808bb476cfdd430
PlayaMarito <1000aromascafe@gmail.com>
2024-01-28 17:21:17 -0500

    Todo OK con variables de entorno

--- api.js
+++ api.js
@@ -1,7 +1,27 @@
 var mongoose = require("mongoose");
 var express = require("express");
 var router = express.Router();
-var query = "mongodb+srv://CarlosDev:1qaz@cluster0.ktxfjrr.mongodb.net/taskBD?retryWrites=true&w=majority"
+
+
+let environment = null;
+
+if (!process.env.ON_HEROKU) {
+    console.log("Cargando variables de entorno desde archivo");
+    const env = require('node-env-file');
+    env(__dirname + '/.env');
+}
+
+environment = {
+    DBMONGOUSER: process.env.DBMONGOUSER,
+    DBMONGOPASS: process.env.DBMONGOPASS,
+    DBMONGOSERV: process.env.DBMONGOSERV,
+    DBMONGO: process.env.DBMONGO,
+};
+
+var query = 'mongodb+srv://' + environment.DBMONGOUSER + ':' + environment.DBMONGOPASS + '@' + environment.DBMONGOSERV + '/' + environment.DBMONGO + '?retryWrites=true&w=majority';
+
+
+// var query = "mongodb+srv://CarlosDev:1qaz@cluster0.ktxfjrr.mongodb.net/taskBD?retryWrites=true&w=majority"
 var TaskModel = require('./task_schema');
 const db = (query);
 
@@ -57,6 +77,16 @@ router.post('/update-task', function (req, res) {
         Name: req.body.Name,
         Deadline: req.body.Deadline
     }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
+router.delete('/delete-task', function (req, res) {
+    TaskModel.deleteOne({ TaskId: req.body.TaskId }, function (err, data) {
         if (err) {
             res.status(500).send("Internal error\n");
         } else {
